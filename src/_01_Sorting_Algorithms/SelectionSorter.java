e3806051053a0a340fe088caa00bbec03761b3f1
league <league@localhost>
2024-05-25 12:03:41 -0700

    Remove implementation of SelectionSorter
    Remove the implementation but leave the instructions.

--- src/_01_Sorting_Algorithms/SelectionSorter.java
+++ src/_01_Sorting_Algorithms/SelectionSorter.java
@@ -19,28 +19,23 @@ public class SelectionSorter extends Sorter {
     void sort(int[] array, SortingVisualizer display) {
         // 1. make a for loop to iterate through all but the last
         //    element of the array
-        for( int i = 0; i < array.length - 1; i++ ) {
+
 
             // 2. create an integer called index and set it equal to i
-            int index = i;
+
 
             // 3. make another for loop that starts at i + 1 and
             //    goes through the entire array
-            for( int k = i + 1; k < array.length; k++ ) {
+
 
                 // 4. if the array element at k is less than the
                 //    element at index, then set index equal to k
-                if( array[k] < array[index] ) {
-                    index = k;
-                }
-            }
+
+            
             
             // 5. swap the array element at index with the array element at i
-            int temp = array[i];
-            array[i] = array[index];
-            array[index] = temp;
             
-            display.updateDisplay();
-        }
+            // 6. Update the display
+        
     }
 }
