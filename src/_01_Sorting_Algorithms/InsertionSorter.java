02e9e99b2505ba28cde29659120a97f54b290b2f
league <league@localhost>
2024-05-25 11:53:04 -0700

    Remove implementation of InsertionSorter
    Remove the implementation but leave the instructions

--- src/_01_Sorting_Algorithms/InsertionSorter.java
+++ src/_01_Sorting_Algorithms/InsertionSorter.java
@@ -17,23 +17,16 @@ public class InsertionSorter extends Sorter {
     @Override
     void sort(int[] array, SortingVisualizer display) {
         // 1. make a for loop that starts at 1 and goes through 
-        //  the length of the array 
-        for( int i = 1; i < array.length; i++ ) {
+        //  the length of the array
+    	//  Update the display before this loop repeats
+
 
             // 2. make another for loop that starts at i and counts down
             //    while j is greater than 0
-            for( int j = i ; j > 0 ; j-- ) {
+
 
                 // 3. if the element at j is less than the element at j - 1,
                 //    then swap them
-                if( array[j] < array[j - 1 ]) {
-                    int temp = array[j];
-                    array[j] = array[j - 1];
-                    array[j - 1] = temp;
-                }
-            }
-            
-            display.updateDisplay();
-        }
+
     }
 }
